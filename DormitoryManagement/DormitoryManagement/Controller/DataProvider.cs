4fbb72ca3b7acff055272796706e079c0f6ab91d
hohuyhoangg <hoangho1147@gmail.com>
2020-11-19 22:52:07 +0700

    add DataProvider + ExcelFIle +ExcelHelper + FormatData

---
+++ DormitoryManagement/DormitoryManagement/Controller/DataProvider.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DormitoryManagement.Controller
+{
+    public static class DataProvider
+    {
+        private static string connectionString = @"";
+        public static DataTable ExcuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] ListPara = query.Split(' ');// cắt chuỗi theo khoảng trắng
+                    int i = 0;
+                    foreach (string item in ListPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                adapter.Fill(data);
+
+                connection.Close();
+            }
+            return data;
+        }
+        public static int ExcuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] ListPara = query.Split(' ');// cắt chuỗi theo khoảng trắng
+                    int i = 0;
+                    foreach (string item in ListPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            return data;
+        }
+        public static object ExcuteScalar(string query, object[] parameter = null)
+        {
+            object data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] ListPara = query.Split(' ');// cắt chuỗi theo khoảng trắng
+                    int i = 0;
+                    foreach (string item in ListPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = command.ExecuteScalar();
+
+                connection.Close();
+            }
+            return data;
+        }
+    }
+}
