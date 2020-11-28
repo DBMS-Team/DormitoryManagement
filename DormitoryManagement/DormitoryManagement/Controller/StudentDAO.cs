7c29dcf6871bbc59499cc4031e8be94cd90187e3
hohuyhoangg <hoangho1147@gmail.com>
2020-11-28 14:38:48 +0700

    update database + add code StudentDAO

--- DormitoryManagement/DormitoryManagement/Controller/StudentDAO.cs
+++ DormitoryManagement/DormitoryManagement/Controller/StudentDAO.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using DormitoryManagement.Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DormitoryManagement.Controller
 {
-    public class StudentDAO
+    public static class StudentDAO
     {
+        public static StudentDTO GetStudentById(string id)
+        {
+            string query = string.Format("EXEC dbo.USP_GetStudentById @USER_ID = {0}", id);
+            DataTable dataTable = DataProvider.ExcuteQuery(query);
+            foreach (DataRow item in dataTable.Rows)
+            {
+                return new StudentDTO(item);
 
+            }
+            return null;
+        }
     }
 }
--- DormitoryManagement/DormitoryManagement/Controller/UserDAO.cs
+++ DormitoryManagement/DormitoryManagement/Controller/UserDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DormitoryManagement.Model;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -11,9 +12,30 @@ namespace DormitoryManagement.Controller
     {
         public static bool Login(string userName, string passWord)
         {
-            string query = string.Format("EXEC dbo.USP_Login @USERNAME = N'{0}',@PASSWORD = N'{1}'", userName, passWord);
+            string query = string.Format("EXEC dbo.USP_Login @USERNAME = '{0}',@PASSWORD = '{1}'", userName, passWord);
             DataTable result = DataProvider.ExcuteQuery(query);
             return result.Rows.Count > 0;
+        }
+        public static UserDTO GetUserByUsername(string userName)
+        {
+            string query = string.Format("EXEC dbo.USP_GetUserByUsername @USERNAME = '{0}'",userName);
+            DataTable dataTable = DataProvider.ExcuteQuery(query);
+            foreach (DataRow item in dataTable.Rows)
+            {
+                return new UserDTO(item);
+            }
+            return null;
+        }
+        public static UserDTO GetUserById(string id)
+        {
+            string query = string.Format("EXEC dbo.USP_GetUserById @USER_ID = {0}", id);
+            DataTable dataTable = DataProvider.ExcuteQuery(query);
+            foreach (DataRow item in dataTable.Rows)
+            {
+                return new UserDTO(item);
+
+            }
+            return null;
         }
     }
 }
