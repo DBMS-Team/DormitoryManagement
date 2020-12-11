6df6d7a80b6b6626c6060cf68ae9f948c4da6949
Khanh Lam <quockhanhdev@gmail.com>
2020-12-11 16:42:37 +0700

    Update Bill #16:42 11/12/2020

--- DormitoryManagement/DormitoryManagement/Controller/BillDAO.cs
+++ DormitoryManagement/DormitoryManagement/Controller/BillDAO.cs
@@ -1,46 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data;
-using DormitoryManagement.Model;
 
 namespace DormitoryManagement.Controller
 {
-    public static class BillDAO
-    {
-        public static bool AddBill(long Employee_ID, string Room_Name, string Sector_Name, DateTime CreatDay, string Month, string Year, string Status, string Total)
-        {
-            string CREATEDAY = Convert.ToDateTime(CreatDay).ToString("yyyyMMdd");
-            string query = string.Format("EXEC dbo.USP_InsetBill @Employee_ID = {0}," +
-                                                                "@Room_Name = N'{1}'," +
-                                                                "@Sector_Name = N'{2}'," +
-                                                                "@Create_date = '{3}'," +
-                                                                "@Month = {4}," +
-                                                                "@Year = {5}, " +
-                                                                "@Status = {6}," +
-                                                                "@total ={7} ", Employee_ID, Room_Name,Sector_Name, CREATEDAY,Month,Year,Status,Total);
-            int result = DataProvider.ExcuteNonQuery(query);
-            return result > 0;
-        }
-        public static DataTable GetListBillView()
-        {
-            string query = string.Format("EXEC dbo.USP_GetListBillView");
-            DataTable dataTable = DataProvider.ExcuteQuery(query);
-            return dataTable;
-        }
-        public static DataTable GetListBillViewByBuldingAndRoom(string bulding, string room)
-        {
-            string query = string.Format("EXEC dbo.USP_GetListBillViewBySectorAndRoom @SECTOR_NAME = N'{0}', @ROOM_ID = N'{1}'", bulding, room);
-            DataTable dataTable = DataProvider.ExcuteQuery(query);
-            return dataTable;
-        }
-        public static DataTable GetListBillViewByStatus(bool status)
-        {
-            string query = string.Format("EXEC dbo.USP_GetListBillViewByStatus @STATUS = {0}", status);
-            DataTable dataTable = DataProvider.ExcuteQuery(query);
-            return dataTable;
-        }
-    }
+   public static class BillDAO
+   {
+      public static bool AddBill(long Employee_ID, string Room_Name, string Sector_Name, DateTime CreatDay, string Month, string Year, string Status, string Total)
+      {
+         string CREATEDAY = Convert.ToDateTime(CreatDay).ToString("yyyyMMdd");
+         string query = string.Format("EXEC dbo.USP_InsetBill @Employee_ID = {0}," +
+                                                             "@Room_Name = N'{1}'," +
+                                                             "@Sector_Name = N'{2}'," +
+                                                             "@Create_date = '{3}'," +
+                                                             "@Month = {4}," +
+                                                             "@Year = {5}, " +
+                                                             "@Status = {6}," +
+                                                             "@total ={7} ", Employee_ID, Room_Name, Sector_Name, CREATEDAY, Month, Year, Status, Total);
+         int result = DataProvider.ExcuteNonQuery(query);
+         return result > 0;
+      }
+
+      public static DataTable GetListBillView()
+      {
+         string query = string.Format("EXEC dbo.USP_GetListBillView");
+         DataTable dataTable = DataProvider.ExcuteQuery(query);
+         return dataTable;
+      }
+
+      public static DataTable GetListBillViewByBuldingAndRoom(string bulding, string room)
+      {
+         string query = string.Format("EXEC dbo.USP_GetListBillViewBySectorAndRoom @SECTOR_NAME = N'{0}', @ROOM_ID = N'{1}'", bulding, room);
+         DataTable dataTable = DataProvider.ExcuteQuery(query);
+         return dataTable;
+      }
+
+      public static DataTable GetListBillViewByStatus(bool status)
+      {
+         string query = string.Format("EXEC dbo.USP_GetListBillViewByStatus @STATUS = {0}", status);
+         DataTable dataTable = DataProvider.ExcuteQuery(query);
+         return dataTable;
+      }
+   }
 }
