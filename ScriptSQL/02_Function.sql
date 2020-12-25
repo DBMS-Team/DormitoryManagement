d5fb45b107bf30b4925fa6923f8c31270b1e550f
Khanh Lam <quockhanhdev@gmail.com>
2020-12-25 22:08:20 +0700

    Update 02_Function.sql

--- ScriptSQL/02_Function.sql
+++ ScriptSQL/02_Function.sql
@@ -155,18 +155,17 @@ AS
 BEGIN 
 	IF @strInput IS NULL RETURN @strInput 
 	IF @strInput = '' RETURN @strInput 
-	DECLARE @RT NVARCHAR(4000) 
-	DECLARE @SIGN_CHARS NCHAR(136) 
-	DECLARE @UNSIGN_CHARS NCHAR (136) 
+	
+	DECLARE @SIGN_CHARS NCHAR(136), @UNSIGN_CHARS NCHAR (136)
+	
 	SET @SIGN_CHARS = N'ăâđêôơưàảãạáằẳẵặắầẩẫậấèẻẽẹéềểễệế ìỉĩịíòỏõọóồổỗộốờởỡợớùủũụúừửữựứỳỷỹỵý 
 	ĂÂĐÊÔƠƯÀẢÃẠÁẰẲẴẶẮẦẨẪẬẤÈẺẼẸÉỀỂỄỆẾÌỈĨỊÍ ÒỎÕỌÓỒỔỖỘỐỜỞỠỢỚÙỦŨỤÚỪỬỮỰỨỲỶỸỴÝ' + NCHAR(272)+ NCHAR(208) -- hai chữ Đ
 	SET @UNSIGN_CHARS = N'aadeoouaaaaaaaaaaaaaaaeeeeeeeeee iiiiiooooooooooooooouuuuuuuuuuyyyyy 
 	AADEOOUAAAAAAAAAAAAAAAEEEEEEEEEEIIIII OOOOOOOOOOOOOOOUUUUUUUUUUYYYYYDD' 
-	DECLARE @COUNTER int 
-	DECLARE @COUNTER1 INT 
-	SET @COUNTER = 1 
+	
+	DECLARE @COUNTER int = 1, @COUNTER1 INT
 
-	WHILE (@COUNTER <=LEN(@strInput)) 
+	WHILE (@COUNTER <= LEN(@strInput)) 
 	BEGIN 
 		SET @COUNTER1 = 1 
 		WHILE (@COUNTER1 <=LEN(@SIGN_CHARS)+1) 
@@ -179,9 +178,9 @@ BEGIN
 				SET @strInput = SUBSTRING(@strInput, 1, @COUNTER-1) + SUBSTRING(@UNSIGN_CHARS, @COUNTER1,1) + SUBSTRING(@strInput, @COUNTER+1,LEN(@strInput)- @COUNTER) 
 				BREAK 
 			END 
-		SET @COUNTER1 = @COUNTER1 +1 
+		SET @COUNTER1 = @COUNTER1 + 1 
 		END 
-		SET @COUNTER = @COUNTER +1 
+		SET @COUNTER = @COUNTER + 1 
 	END 
 
 	--- xóa dấu cách thừa
